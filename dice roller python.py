747cdb2e8cd7ded8650256ec6df944fdfab10d2b
SharmaAddy <142694156+SharmaAddy@users.noreply.github.com>
2023-11-09 22:56:30 -0600

    Update dice roller python.py
    Added better print statement for easy understanding.

--- dice roller python.py
+++ dice roller python.py
@@ -5,7 +5,7 @@ while True:
   print("Devil is rolling the dices......")
   time.sleep(1)
   print("Devil says: ", random.randint(1,6), "and",random.randint(1,12))
-  repeat=input("Roll again? y/n")
+  repeat=input("Do you want to roll again? y/n")
   if repeat=="y" or "Y":
     continue
   elif repeat=="n" or "N":
